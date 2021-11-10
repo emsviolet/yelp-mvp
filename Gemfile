ff22d0eb31e792313ba4d01b4c21edbf822ee86b
Emily Millard <emily_millard@live.co.uk>
2021-11-10 16:49:58 +0100

    feat: restaurant/review model, controllers

--- Gemfile
+++ Gemfile
@@ -21,6 +21,9 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'faker'
+
+gem 'simple_form'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
@@ -43,3 +46,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+category = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
+puts 'Cleaning database'
+Restaurant.destroy_all
+
+puts 'Creating database'
+5.times do
+  restaurant = Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.street_address,
+                                  phone_number: Faker::PhoneNumber.cell_phone, category: category.sample)
+  puts "#{restaurant.name} created"
+end
